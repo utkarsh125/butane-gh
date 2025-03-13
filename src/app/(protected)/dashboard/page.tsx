8ef3277f70dc344b40a5a73a5544bd98b386fbe0
Utkarsh Pandey <utk2665@gmail.com>
2025-03-13 18:18:38 +0530

    Figuring out AiSummarise

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,15 +1,51 @@
 'use client'
 
+import { ExternalLink, Github } from 'lucide-react';
+
+import Link from 'next/link';
 import React from 'react'
+import { useProjects } from '@/hooks/use-project';
 import { useSession } from 'next-auth/react';
 
 const DashboardPage = () => {
   const { data: session } = useSession();
+  const { project } = useProjects();
   return (
     <div>
-        <div>
-            {session?.user?.name}
+      {project?.id}
+      <div className='flex items-center justify-between flex-wrap gap-y-4'>
+        {/* github link */}
+        <div className='flex items-center w-fit rounded-md bg-primary px-4 py-3'>
+          <Github className='size-5 text-white'/>
+          <div className='ml-2'>
+            <p className='text-sm font-medium text-white'>
+              This project is linked to {' '}
+              <Link href={project?.githubUrl ?? ''} className='inline-flex items-center text-white/80 hover:underline'>
+                {project?.githubUrl}
+                <ExternalLink className='ml-1 size-4'/>
+              </Link>
+            </p>
+          </div>
         </div>
+
+        <div className='h-4'></div>
+
+        <div className="flex items-center gap-4">
+          TeamMembers
+          InviteButton 
+          ArchiveButton 
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-5">
+          AskQuestionCard 
+          MeetingCard
+        </div>
+      </div>
+
+      <div className="mt-8"></div>
+      CommitLog
     </div>
   )
 }
---
+++ src/lib/github.ts
@@ -0,0 +1,114 @@
+//This file will take care of Github things via Octokit
+
+import { AiSummariseCommit } from "./gemini";
+import { Octokit } from "octokit";
+import axios from "axios";
+import { db } from "@/server/db";
+
+export const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+const githubUrl = "https://github.com/docker/genai-stack";
+
+type Response = {
+  commitHash: string;
+  commitMessage: string;
+  commitAuthorName: string;
+  commitAuthorAvatar: string;
+  commitDate: string;
+};
+
+export const getCommitHashes = async (
+  githubUrl: string,
+): Promise<Response[]> => {
+  // const [owner, repo] = githubUrl.split('/').slice(-2);
+  // const response = await octokit.request(`GET /repos/${owner}/${repo}/commits`);
+  // return response.data.map((commit) => ({
+  //     commitHash: commit.sha,
+  //     commitMessage: commit.commit.message,
+  // }))
+
+  const [owner, repo] = githubUrl.split('/').slice(-2);
+
+  if(!owner || !repo){
+    throw new Error("Invalid Github URL")
+  }
+
+  const { data } = await octokit.rest.repos.listCommits({
+    owner,
+    repo
+  });
+  const sortedCommits = data.sort(
+    (a: any, b: any) =>
+      new Date(b.commit.author.date).getTime() -
+      new Date(a.commit.author.date).getTime(),
+  );
+
+  return sortedCommits.slice(0, 15).map((commit: any) => ({
+    commitHash: commit.sha as string,
+    commitMessage: commit.commit.message ?? "",
+    commitAuthorName: commit.commit?.author?.name ?? "",
+    commitAuthorAvatar: commit?.author?.avatar_url ?? "",
+    commitDate: commit.commit?.author.date ?? "",
+  }))
+
+  
+};
+
+export const pollCommits = async(projectId: string )=> {
+    const {project, githubUrl} = await fetchProjectGithubUrl(projectId)
+
+    const commitHashes = await getCommitHashes(githubUrl);
+
+    const unprocessedCommits = await filterUnprocessedCommits(projectId, commitHashes);
+    // console.log(unprocessedCommits);
+    return unprocessedCommits
+
+    
+}
+
+
+//TODO: Find a way to push the diffs into GeminiAPI.
+async function summariseCommits(githubUrl: string, commitHash: string){
+
+    //get the diff
+    //then pass the diff into AI
+
+    const { data } = await axios.get(`${githubUrl}/commit/${commitHash}.diff`, {
+        headers:{
+            Accept: 'application/vnd.github.v3.diff'
+        }
+    })
+
+    return await AiSummariseCommit(data) || "";
+    
+
+}
+
+async function fetchProjectGithubUrl(projectId: string){
+    const project = await db.project.findUnique({
+        where: {id: projectId},
+        select: {
+            githubUrl: true, 
+        }
+    })
+
+    if(!project?.githubUrl){
+        throw new Error("Project has no Github URL")
+    }
+
+    return {project, githubUrl: project?.githubUrl}
+}
+
+async function filterUnprocessedCommits(projectId: string, commitHashes: Response[]){
+
+    const processedCommits = await db.commit.findMany({
+        where: { projectId }
+    })
+
+    const unprocessedCommits = commitHashes.filter((commit) => !processedCommits.some((processedCommit) => processedCommit.commitHash === commit.commitHash))
+    return unprocessedCommits;
+}
+
+await pollCommits('cm87b26jy0000djvidoa4mmkq').then(console.log)
